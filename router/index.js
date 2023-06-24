3ec88bc52be12ce4e80958af03818b2f14fe304c
Rajan <rajarajan2k1@gmail.com>
2023-06-24 22:22:01 +0530

    add auth api

--- router/index.js
+++ router/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const User = require("../model");
 const bcrypt  = require('bcryptjs');
+const generateToken = require("../utils");
+const verifyToken = require("../middleware");
 
 
 
@@ -26,6 +28,29 @@ router.post('/user',async(req,res)=>{
 
   res.status(404).json({message:"User already Exists"})
 
+})
+
+//authenticate user api
+router.post("/authenticate",async(req,res)=>{
+  const {email,password} = req.body;
+  const user = await User.findOne({email});
+  if(!user){
+    return res.status(404).json({message:"User not found"});
+  }
+  const isMatch = await bcrypt.compare(password,user.password);
+
+  if(!isMatch){
+    return res.status(401).json({message:"incorrect Password"});
+  }
+
+  const token = generateToken(user);
+  res.json({token});
+})
+
+
+//Protected data api
+router.get("/data",verifyToken,(req,res)=>{
+  res.json({message:`welcome,${req.user.email}! This is protected data`})
 })
 
 
