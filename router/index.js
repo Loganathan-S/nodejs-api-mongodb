7b6a99f956f8735e5741461d7e1e59e1ea8565e4
Rajan <rajarajan2k1@gmail.com>
2023-06-26 11:39:43 +0530

    add nodemailer

--- router/index.js
+++ router/index.js
@@ -3,7 +3,7 @@ const User = require("../model");
 const bcrypt  = require('bcryptjs');
 const generateToken = require("../utils");
 const verifyToken = require("../middleware");
-
+const nodemalier = require("nodemailer");
 
 
 const router = express.Router();
@@ -52,6 +52,42 @@ router.post("/authenticate",async(req,res)=>{
 router.get("/data",verifyToken,(req,res)=>{
   res.json({message:`welcome,${req.user.email}! This is protected data`})
 })
+
+
+// reset-password api
+router.post("/reset-password",async(req,res)=>{
+  const {email} = req.body;
+  const user = await User.findOne({email});
+
+  if(!user){
+    return res.status(404).json({message:"user not found"})
+  }
+
+  const token = Math.random().toString(36).slice(-8)
+  user.resetPasswordToken = token;
+  user.resetPasswordExpires = Date.now() + 360000; // 1hour for valid
+  await user.save();
+  const transporter = nodemalier.createTransport({
+    service:"gmail",
+    auth:{
+      user:"rajarajan2k1@gmail.com",
+      pass:"lxtbifszbjrvxsir"
+    },
+  })
+  const message = {
+    from:"rajarajan2k1@gmail.com",
+    to:user.email,
+    subject:"Password reset request",
+    text:`you are receiving this email because you(or someone else) has requested a password reset for your account.\n\n please use the following token to reset your password: ${token}\n\n If you did not request your password  reset, please ignore this email.`
+  }
+  transporter.sendMail(message,(err,info) => {
+    if (err) {
+      res.status(404).json({message:"Something went wrong, try again"})
+    }
+    res.status(200).json({message:"Password reset email sent" + info.response});
+  });
+})
+
 
 
 module.exports = router;
--- utils/index.js
+++ utils/index.js
@@ -1,5 +1,5 @@
 
 const jwt = require('jsonwebtoken');
-const generateToken= (user) => jwt.sign({id:user.id},process.env.SECRET_KEY,{expiresIn:"30sec"});
+const generateToken= (user) => jwt.sign({id:user.id},process.env.SECRET_KEY,{expiresIn:"2m"});
 
 module.exports = generateToken
